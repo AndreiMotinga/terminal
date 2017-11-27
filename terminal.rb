8288ff2fe39fcbbb32e63313aa2a84951251c36a
Andrei Motinga <andrew.motinga@gmail.com>
2017-11-27 12:51:34 -0500

    correct specs
    misread the api design.

---
+++ terminal.rb
@@ -0,0 +1,13 @@
+class Terminal
+
+  attr_reader :pricing_info
+  def initialize(pricing_info)
+    @pricing_info = pricing_info
+  end
+
+  def scan(item)
+  end
+
+  def total
+  end
+end
--- terminal_spec.rb
+++ terminal_spec.rb
@@ -35,28 +35,34 @@ describe Terminal do
     end
 
     it "calculates total" do
-      expect(terminal.scan("A")).to eq 2.00
+      @terminal.scan("A")
+      expect(@terminal.total).to eq 2.00
     end
 
     it "calculates total of multiple items using discount" do
-      expect(terminal.scan("AAAA")).to eq 7.00
+      4.times { @terminal.scan("A") }
+      expect(@terminal.total).to eq 7.00
     end
 
     it "calculates total of multiple items using discount" do
-      expect(terminal.scan("AAAAA")).to eq 9.00
+      5.times { @terminal.scan("A") }
+      expect(@terminal.total).to eq 9.00
     end
 
-    # Code challenge tests
+    # Code challenge required tests
     it "calculates total of multiple items" do
-      expect(terminal.scan("ABCDABAA")).to eq 32.40
+      "ABCDABAA".chars.each { |char| @terminal.scan(char) }
+      expect(@terminal.total).to eq 32.40
     end
 
     it "uses discounts" do
-      expect(terminal.scan("CCCCCCC")).to eq 7.25
+      "CCCCCCC".chars.each { |char| @terminal.scan(char) }
+      expect(@terminal.total).to eq 7.25
     end
 
     it "counts discounts with duplicated items" do
-      expect(terminal.scan("ABCD")).to eq 15.40
+      "ABCD".chars.each { |char| @terminal.scan(char) }
+      expect(@terminal.total).to eq 15.40
     end
   end
 end
