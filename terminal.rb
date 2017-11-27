aa1ef659a8ae8c62fa68d489999f371b6999bd64
Andrei Motinga <andrew.motinga@gmail.com>
2017-11-27 13:04:24 -0500

    refactor specs
    use symbols instead of strings
    use current tests for #total instead of #scan
    add proper test for #scan
    implement #scan

--- terminal.rb
+++ terminal.rb
@@ -1,11 +1,13 @@
 class Terminal
 
-  attr_reader :pricing_info
+  attr_reader :pricing_info, :items
   def initialize(pricing_info)
     @pricing_info = pricing_info
+    @items = Hash.new(0)
   end
 
   def scan(item)
+    @items[item.to_sym] += 1
   end
 
   def total
--- terminal_spec.rb
+++ terminal_spec.rb
@@ -1,37 +1,48 @@
 require_relative "terminal"
 
 describe Terminal do
-  describe "#scan" do
-    before do
-      pricing_info = {
-        "A": {
-          price: 2.00,
-          "disounts": [
-            {
-              count: 4,
-              price: 7.00
-            }
-          ]
-        },
-        "B": {
-          price: 12.00,
-          disounts: []
-        },
-        "C": {
-          price: 1.25,
-          disounts: [
-            {
-              count: 6,
-              price: 6.00
-            }
-          ]
-        },
-        "D": {
-          price: 0.15,
-          discounts: []
-        }
+  before do
+    pricing_info = {
+      A: {
+        price: 2.00,
+        "disounts": [
+          {
+            count: 4,
+            price: 7.00
+          }
+        ]
+      },
+      B: {
+        price: 12.00,
+        disounts: []
+      },
+      C: {
+        price: 1.25,
+        disounts: [
+          {
+            count: 6,
+            price: 6.00
+          }
+        ]
+      },
+      D: {
+        price: 0.15,
+        discounts: []
       }
-      @terminal = Terminal.new(pricing_info)
+    }
+    @terminal = Terminal.new(pricing_info)
+  end
+  describe "#scan" do
+    it "adds item to @items" do
+      @terminal.scan("A")
+      expect(@terminal.items).to eq({ A: 1 })
+    end
+  end
+
+  describe "#total" do
+    it "calculates total" do
+      @terminal.scan("A")
+      expect(@terminal.total).to eq 2.00
     end
 
     it "calculates total" do
